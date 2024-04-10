5e888ec4da514dbf75605d90ed417a83ddca4ad4
konstantin <kosta123139@gmail.com>
2024-04-10 20:40:48 +0300

    add __main__()

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils.function import get_data
+from config import data_file_name
+
+
+def __main__():
+    data = get_data(data_file_name)
+    pass
+
+
+if __name__ == "__main__":
+    __main__()

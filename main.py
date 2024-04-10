795b07d4dd94c79c8df27cf170327ab7bb0c5b5b
konstantin <kosta123139@gmail.com>
2024-04-10 23:56:37 +0300

    изменил подход полностью еще этап разработки)

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from utils.function import get_data
+from utils.function import sort_status
+from utils.function import sort_dates
+from utils.function import revers
+# from utils.function import compilation_transfer
 from config import data_file_name
 
 
 def __main__():
-    data = get_data(data_file_name)
-    pass
+    list_data = get_data(data_file_name)
+    list_sorted_status = sort_status(list_data)
+    list_sorted_dates = sort_dates(list_sorted_status)
+    last_5_transaction = revers(list_sorted_dates[-5:])
+    for i in last_5_transaction:
+        print(i)
+
+    # l = compilation_transfer(data)
+    # for i in l:
+    #     # print(i.get_data())
+    #     print(i.get_amount())
+    #     # print(i.get_amount_name())
+    #     # print(i.get_description())
+    #     # print(i.get_bank_number())
+    #     # print(i.get_card_number())
 
 
 if __name__ == "__main__":
--- utils/function.py
+++ utils/function.py
@@ -1,4 +1,5 @@
 import json
+# from utils.Transfer import Transfer
 
 
 def get_data(file_name) -> list:
@@ -7,3 +8,33 @@ def get_data(file_name) -> list:
         json_ = file.read()
         json_list = json.loads(json_)
     return json_list
+
+
+def sort_status(json_list: list) -> list:
+    """сохраняет только операции со статусом <<EXECUTED>>"""
+    result = []
+    for i in json_list:
+        if i.get('state') == "EXECUTED":
+            result.append(i)
+    return result
+
+
+def sort_dates(list_: list) -> list:
+    """сортирует по датам от меньшего к большему"""
+    return sorted(list_, key=lambda x: x['date'])
+
+
+def revers(list_: list) -> list:
+    """разворачивает список от большего к меньшему"""
+    return list_[::-1]
+
+
+
+
+# def compilation_transfer(list_: list) -> list:
+#     list_transfer = []
+#     for i in list_:
+#         list_transfer.append(Transfer(i.get('date'), i.get('operationAmount'), i.get('name'), i.get('description'),
+#                                       i.get('to'), i.get('from')))
+#     return list_transfer
+#

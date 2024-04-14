997312a3b277b2382a2258b07d15a62c56935657
konstantin <kosta123139@gmail.com>
2024-04-14 23:37:17 +0300

    украшения в коде

--- src/utils/function.py
+++ src/utils/function.py
@@ -5,9 +5,7 @@ import datetime
 def get_data_json(file_name) -> list:
     """Читает файл json, конвертирует в лист со словарями и возвращает его"""
     with open(file_name, 'r', encoding="utf-8") as file:
-        json_ = file.read()
-        json_list = json.loads(json_)
-    return json_list
+        return json.load(file)
 
 
 def sort_status(json_list: list, state: str) -> list:
